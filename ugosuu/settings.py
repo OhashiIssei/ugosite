361dd310148682e270e0bf85f6bbd49aa43e52fa
大橋一生 <greenman@oohashiisshounoMacBook-Air-2.local>
2023-01-05 14:29:58 +0900

    サムネイル作成機能、検索機能を加えました

--- ugosuu/settings.py
+++ ugosuu/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&0&0&wv2m^%$2s#jiwpo=#-hs#&7qfbj#4v_k^e=uphs9wq@oy'
+# SECRET_KEY = 'django-insecure-&0&0&wv2m^%$2s#jiwpo=#-hs#&7qfbj#4v_k^e=uphs9wq@oy'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&0&0&wv2m^%$2s#jiwpo=#-hs#&7qfbj#4v_k^e=uphs9wq@oy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
